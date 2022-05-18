d0e2fef2798e3707188fa4e5128d46266028b205
LukaszTheLittle <lukis.maly@gmail.com>
2022-05-18 20:19:08 +0200

    Indentation correction

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -2,7 +2,7 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes
-                    val textResId: Int,
-                    val answer: Boolean,
-                    var isAnswered: Boolean = false)
+data class Question(
+    @StringRes val textResId: Int,
+    val answer: Boolean,
+    var isAnswered: Boolean = false)
